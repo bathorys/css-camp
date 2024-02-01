1e5c01259d4ebc75e57767a0dfdbebfd549e587f
이길환 <pos@mz.co.kr>
2024-02-01 15:08:57 +0900

    set vitepress

---
+++ .vitepress/config.mjs
@@ -0,0 +1,38 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: 'CSS camp',
+  description: 'CSS camp',
+  lang: 'ko-KR',
+  srcDir: './src',
+  markdown: {
+    lineNumbers: true
+  },
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    search: {
+      provider: 'local'
+    },
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [{ icon: 'github', link: 'https://github.com/vuejs/vitepress' }]
+  },
+  vite: {
+    css: {
+      devSourcemap: true
+    }
+  }
+})
